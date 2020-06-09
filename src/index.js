723f90cad4d7df563ad2add7d5ad122227815f9f
Bar Horing Amir <bar.apple@gmail.com>
2020-06-09 09:35:33 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+
+const Application = () => {
+  return <div>Hello world</div>;
+};
+
+ReactDOM.render(<Application />, rootElement);
